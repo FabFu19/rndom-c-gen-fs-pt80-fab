f7bb0bf6beee0fb9753789477c030e2fed356183
FabFu19 <fabianfuen19@icloud.com>
2024-10-15 13:55:06 +0000

    feat: random card generator created, settime also created

--- src/app.js
+++ src/app.js
@@ -5,7 +5,59 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//card variables
+const card = document.querySelector("#card");
+const paloUp = document.querySelector("#palo-up");
+const cardValue = document.querySelector("#card-value");
+const paloDown = document.querySelector("#palo-down");
+const palo = ["♥", "♦", "♣", "♠"];
+const cardWord = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "K",
+  "Q"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  getPalo();
+  getCardValue();
+  colorPalo(paloUp.innerText, paloUp.innerText);
+};
+
+//functions
+
+setInterval(() => {
+  getPalo();
+  getCardValue();
+  colorPalo();
+}, 6000);
+
+const getPalo = () => {
+  let gettingPalo = palo[Math.floor(Math.random() * palo.length)];
+  paloUp.innerText = gettingPalo;
+  paloDown.innerText = gettingPalo;
+  cardValue.innerText = gettingPalo;
+};
+const getCardValue = () => {
+  const gettinVal = cardWord[Math.floor(Math.random() * cardWord.length)];
+  cardValue.innerText = gettinVal;
+  //console.log(getCardValue);
+};
+const colorPalo = val => {
+  if (val === "♥" || val === "♦") {
+    paloUp.classList.add("red-color");
+    paloDown.classList.add("red-color");
+    cardValue.classList.add("red-color");
+  }
 };
